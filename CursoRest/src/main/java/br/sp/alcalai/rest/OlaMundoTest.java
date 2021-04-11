6d41dd459cf2e05195f05c4283e310e967843b9e
Agostinho Bernardes <aabernardes@gmail.com>
2021-04-11 18:07:49 -0300

    Modo fluente - Gherkin

---
+++ CursoRest/src/main/java/br/sp/alcalai/rest/OlaMundoTest.java
@@ -0,0 +1,42 @@
+package br.sp.alcalai.rest;
+
+import static io.restassured.RestAssured.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+	
+	@Test
+	public void olaTesteMundo() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		Assert.assertTrue(response.statusCode() == 200);
+		Assert.assertTrue("Status code deveria ser 200", response.statusCode() == 200);
+		Assert.assertEquals(200, response.statusCode());
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+	
+	
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		given() //Pré Condições
+		.when() // Ação
+			.get("http://restapi.wcaquino.me/ola")
+		.then() //Assertivas
+			.statusCode(200)
+			;
+		
+	}	
+
+}
